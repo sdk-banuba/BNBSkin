f71857f41ad44edb619b963279b5f154064a670c
ci <ci@banuba.com>
2024-10-28 19:19:21 +0300

    1.14.1-170-g91aac4bda

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-162-gcda6b9c4b"
+let bnbPackageVersion: Version = "1.14.1-170-g91aac4bda"
 
 let package = Package(
     name: "BNBSkin",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBSkin",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-162-gcda6b9c4b/BNBSkin.zip",
-            checksum: "ccc9739fbeee8cbb352ed69fd0a05ded6ba99f28533df4cc6a8606fdb20c5148"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-170-g91aac4bda/BNBSkin.zip",
+            checksum: "f7bebfe353c68e55c04fbbd721bc3304ff0f258240960681747f71b81f2ea148"
         ),
         .target(
             name: "BNBSkin_BNBSdkCore",
